fdf1ce7b5569a81dba45e5379290933711468635
lucia-231618 <luciaenguidanos23@gmail.com>
2026-03-02 09:55:05 +0100

    Añadir código

--- FirstPerson_RPMI/Assets/_FPS_RPMI/Scripts/Player/FP_Controller.cs
+++ FirstPerson_RPMI/Assets/_FPS_RPMI/Scripts/Player/FP_Controller.cs
@@ -11,11 +11,11 @@ public class FP_Controller : MonoBehaviour
     [SerializeField] float crouchSpeed = 3f;
     [SerializeField] float sprintSpeed = 8f;
     [SerializeField] float maxForce = 1f; //Fuerza maxima de aceleración
-    [SerializeField] float sensitivity = 0.1f;
+    [SerializeField] float sensitivity = 0.1f; //Sensibilidad del ratón
 
     [Header("Player State Bools")]
     [SerializeField] bool isSprinting;
-    [SerializeField] bool isGrouching;
+    [SerializeField] bool isCrouching;
     #endregion
 
     //Variables de autoreferencia
@@ -46,6 +46,45 @@ public class FP_Controller : MonoBehaviour
         
     }
 
+    private void FixedUpdate()
+    {
+        Movement();
+    }
+    private void LateUpdate()
+    {
+        CameraLook();
+    }
+
+    void CameraLook()
+    {
+        //Rotación del personaje /horizontal)
+        transform.Rotate(Vector3.up * lookInput.x * sensitivity);
+        //Rotación de la cámara (vertical)
+        lookRotation += (-lookInput.y * sensitivity);
+        lookRotation = Mathf.Clamp(lookRotation, -90, 90);
+        camHolder.transform.localEulerAngles = new Vector3(lookRotation, 0f, 0f);
+    }
+
+    void Movement()
+    {
+        //Definir los dos vectores que permiten la aceleración
+        Vector3 currentVelocity = rb.linearVelocity;
+        Vector3 targetVelocity = new Vector3(moveInput.x, 0, moveInput.y);
+        //A la dirección a alcanzar le multiplicamos la velocidad
+        targetVelocity *= isCrouching ? crouchSpeed : isSprinting ? sprintSpeed : speed;
+
+        //Convertir la dirección al eje mundial (de Local a World)
+        targetVelocity = transform.TransformDirection(targetVelocity);
+
+        //Calcular el cambio de velocidad (aceleración)
+        Vector3 velocityChange = (targetVelocity - currentVelocity);
+        velocityChange = new Vector3(velocityChange.x, 0, velocityChange.z);
+        velocityChange = Vector3.ClampMagnitude(velocityChange, maxForce);
+
+        //Aplicación del movimiento (DIRECCIÓN + ACELERACIÓN)
+        rb.AddForce(velocityChange, ForceMode.VelocityChange);
+    }
+
     #region Input Method
     public void OnMove(InputAction.CallbackContext context)
     {
@@ -54,7 +93,7 @@ public class FP_Controller : MonoBehaviour
 
     public void OnLook(InputAction.CallbackContext context)
     {
-        moveInput = context.ReadValue<Vector2>();
+        lookInput = context.ReadValue<Vector2>();
     }
 
     public void OnJump(InputAction.CallbackContext context)
